26e0c30d9151ea255abfa2aeb961b756f0114615
omkarvijay5 <omkarvijay5@gmail.com>
2014-07-14 13:37:01 +0530

    added validation for email field to enter a registered email

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,13 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from django import forms
-User = get_user_model()
+from django.shortcuts import get_object_or_404
 
 class FriendEmailForm(forms.Form):
 	friend_email = forms.EmailField(required=True)
+
+	def clean_friend_email(self):
+		email = self.cleaned_data['friend_email']
+		try:
+			user = get_object_or_404(User,email=email)
+		except:
+			raise forms.ValidationError("Enter a registered email")
