d3dd2a06d4e6861305dd1466784f792a02716cf9
omkarvijay5 <omkarvijay5@gmail.com>
2014-07-21 15:31:20 +0530

    added validation when paid user and selected user is same

--- users/forms.py
+++ users/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 from users.models import Friendship, Transaction
+import pdb
 
 class FriendEmailForm(forms.Form):
     friend_email = forms.EmailField(required=True)
@@ -27,12 +28,12 @@ def get_users(current_user):
 
 class SplitBillForm(forms.Form):
     def __init__(self,*args, **kwargs):
-        current_user = kwargs.pop('current_user')
+        self.user = kwargs.pop('current_user')
         super(SplitBillForm, self).__init__(*args, **kwargs)
-        self.fields['paid_user'] = forms.ChoiceField(choices=get_users(current_user), required=False)
+        self.fields['paid_user'] = forms.ChoiceField(choices=get_users(self.user))
         self.fields['paid_user'].label = "Who paid?"
-        self.fields['friends'] = forms.MultipleChoiceField( choices=get_users(current_user), 
-                                                            widget=forms.CheckboxSelectMultiple(), required=False
+        self.fields['friends'] = forms.MultipleChoiceField( choices=get_users(self.user), 
+                                                            widget=forms.CheckboxSelectMultiple()
                                                         )
         self.fields['friends'].label = "Friends whom you want to share"
     item = forms.CharField(label="For What?", 
@@ -44,6 +45,16 @@ class SplitBillForm(forms.Form):
 
     class Meta:
         model = Transaction
+
+    def clean(self):
+        cleaned_data = super(SplitBillForm, self).clean()
+        friends = cleaned_data.get('friends')
+        paid_user = cleaned_data.get('paid_user')
+        user = self.user.username
+        if friends:
+            if len(friends) == 1 and paid_user == user and user in friends:
+                raise forms.ValidationError('You cannot add bill to yourself. Please select atleast one friend')
+        return cleaned_data
 
 
         
