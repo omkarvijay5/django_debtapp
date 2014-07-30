05d1a55ea97d4c56e16c3bc0fe50eee2da832700
omkarvijay5 <omkarvijay5@gmail.com>
2014-07-30 16:23:04 +0530

    added static urls when production

--- django_debtapp/urls.py
+++ django_debtapp/urls.py
@@ -19,3 +19,10 @@ if settings.DEBUG:
             'document_root': settings.MEDIA_ROOT,
         }),
 )
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.STATIC_ROOT,
+        }),
+)
