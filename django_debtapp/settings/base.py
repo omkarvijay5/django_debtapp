92ae300189dfcd7ac5c9961e8eaf45707678e511
omkarvijay5 <omkarvijay5@gmail.com>
2014-07-31 15:52:36 +0530

    added static files dirs

--- django_debtapp/settings/base.py
+++ django_debtapp/settings/base.py
@@ -101,3 +101,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.tz",
     "django.contrib.messages.context_processors.messages"
 )
+
+STATICFILES_DIRS = (
+    os.path.abspath('static'),
+    )
