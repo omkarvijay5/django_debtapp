2d4ee393ebe9f247969c115e553870bf44f2149a
omkarvijay5 <omkarvijay5@gmail.com>
2014-07-31 18:11:05 +0530

    testing static file settings

--- django_debtapp/settings/base.py
+++ django_debtapp/settings/base.py
@@ -17,8 +17,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(''))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY ='test'
-
+SECRET_KEY = 'abc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
