ef0e99c958ed757ed96a28591569d1eb90cb14e1
omkarvijay5 <omkarvijay5@gmail.com>
2014-07-31 15:54:30 +0530

    fixed upload image bug with js and template

--- users/static/users/js/upload_image.js
+++ users/static/users/js/upload_image.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
-    $(".comment-button").click(function(e){
-    	e.preventDefault();
+    $(".upload-image").click(function(){
         username = $('#username').text()
         image_url = 'users/'+username+'/upload/image/'
-        image_form = $('.form-horizontal').text()
-        $('#form-modal-body').load(image_url);
-        $('#form-modal').modal('toggle');
+        $('#form-modal-body').load(image_url)
+        $('#form-modal').modal('toggle')
     });
 });
+
